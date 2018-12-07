ec19c70cd0808ad31a8235977d89f0e5e4ec3473
Bruce Hinrichs <register@oaksavannasoftware.com>
2018-12-07 08:10:14 -0600

    Add project files.

---
+++ BLHMVCStart/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using BLHMVCStart.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BLHMVCStart.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult EnterUserData()
+        {
+            return View("EnterUserData");
+        }
+
+        [HttpPost]
+        public ActionResult ShowUserData(string Firstname, string Lastname, string EmailAddress, string DateOfBirth)
+        {
+            var mdl = new UserDataModel();
+            mdl.FirstName = Firstname;
+            mdl.LastName = Lastname;
+            mdl.EmailAddress = EmailAddress;
+            mdl.DateOfBirth = DateOfBirth;
+            return View(mdl);
+        }
+    }
+}
---
+++ BLHMVCStart/Models/UserDataModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+
+namespace BLHMVCStart.Models
+{
+    public class UserDataModel
+    {
+        [DisplayName("First Name")]
+        public string FirstName { get; set;  }
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName("Email")]
+        public string EmailAddress { get; set; }
+
+        [DisplayName("Date Of Birth")]
+        public string DateOfBirth { get; set; }
+    }
+}
